ae1ff212a44fb67bbe3ea526a730e770c72633df
oscarsanchez <oscarsanchez@unitec.edu>
2016-06-21 14:17:03 -0600

    metodos para manejar archivos
    en la interfaz de dataserver

--- ExploradorArchivos/src/exploradorarchivos/DataServer.java
+++ ExploradorArchivos/src/exploradorarchivos/DataServer.java
@@ -5,6 +5,7 @@
  */
 package exploradorarchivos;
 
+import java.io.File;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
@@ -14,4 +15,14 @@ import java.rmi.RemoteException;
  */
 public interface DataServer extends Remote {
     public void Mensaje(String mensaje)throws RemoteException;
+    
+    public File getFile(String name) throws RemoteException;
+
+    public String getFileContent(String name) throws RemoteException;
+
+    public boolean createFile(String content, String name) throws RemoteException;
+
+    public boolean deleteFile(String name) throws RemoteException;
+
+    public boolean deleteDir(String name) throws RemoteException;
 }
--- ExploradorArchivos/src/exploradorarchivos/DataServer1.java
+++ ExploradorArchivos/src/exploradorarchivos/DataServer1.java
@@ -5,7 +5,13 @@
  */
 package exploradorarchivos;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.rmi.RemoteException;
@@ -25,6 +31,14 @@ public class DataServer1 extends UnicastRemoteObject implements DataServer {
         super();
         this.directorio = directorio;
         
+    }
+    
+    public File getDataDirectory() {
+        return directorio;
+    }
+
+    public void setDataDirectory(File dataDirectory2) {
+        directorio = dataDirectory2;
     }
      
     @Override
@@ -60,7 +74,98 @@ public class DataServer1 extends UnicastRemoteObject implements DataServer {
         }
     }
     
-    
+    @Override
+    public File getFile(String name) throws RemoteException {
+        for (final File fileEntry : directorio.listFiles()) {
+            if (fileEntry.getName().equals(name)) {
+                return fileEntry;
+            }
+        }
+        System.err.println("No se encontro el archivo");
+        return null;
+    }
+
+    @Override
+    public boolean createFile(String content, String name) throws RemoteException {
+        PrintWriter writer = null;
+        String[] path = name.split("/");
+        name = directorio.getAbsolutePath() + "\\";
+        for (int i = 1; i < path.length; i++) {
+            name += path[i];
+            if (name.endsWith(".txt")) {
+                try {
+                    writer = new PrintWriter(name, "UTF-8");
+                } catch (FileNotFoundException | UnsupportedEncodingException ex) {
+                    Logger.getLogger(DataServer1.class.getName()).log(Level.SEVERE, null, ex);
+                    return false;
+                }
+                writer.println(content);
+                writer.close();
+            } else {
+                File newFile = new File(name);
+                name += "\\";
+                if (!newFile.exists()) {
+                    newFile.mkdir();
+                }
+            }
+        }
+
+        System.out.println("Nombre del Archivo: " + name);
+        /*
+        
+         */
+        return true;
+    }
+
+    @Override
+    public boolean deleteFile(String name) throws RemoteException {
+        name = directorio.getAbsolutePath() + "\\" + name;
+        File file = new File(name);
+        return file.delete();
+    }
+
+    @Override
+    public String getFileContent(String name) throws RemoteException {
+        String collectedInfo = "";
+        try {
+            String line = "";
+            name = directorio.getAbsolutePath() + "\\" + name;
+            FileReader f = new FileReader(name);
+            try (BufferedReader b = new BufferedReader(f)) {
+                while ((line = b.readLine()) != null) {
+                    collectedInfo += line;
+                }
+            }
+        } catch (IOException ex) {
+        }
+        return collectedInfo;
+    }
+
+    private void delete(File f) throws IOException {
+        if (f.isDirectory()) {
+            for (File c : f.listFiles()) {
+                delete(c);
+            }
+        }
+        if (!f.delete()) {
+            throw new FileNotFoundException("Failed to delete file: " + f);
+        }
+    }
+
+    @Override
+    public boolean deleteDir(String name) throws RemoteException {
+        name = directorio.getAbsolutePath() + "\\" + name;
+        File dir = new File(name);
+        if (dir.exists()) {
+            try {
+                delete(dir);
+            } catch (IOException ex) {
+                return false;
+                //Logger.getLogger(DataServer2.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return true;
+    }
 
     
 }
